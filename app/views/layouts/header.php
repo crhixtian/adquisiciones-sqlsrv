fa45973c0981dc5baaeb84ffe0782c8b689543ce
crhixtian <crhistian.villantoy@hotmail.com>
2026-02-28 01:12:31 -0500

    instance vm gcs

--- app/views/layouts/header.php
+++ app/views/layouts/header.php
@@ -6,7 +6,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Sistema MVC - PECH</title>
+    <title>Adquisiciones - PECH</title>
     <link href="https://cdn.jsdelivr.net/npm/@tabler/core@1.4.0/dist/css/tabler.min.css" rel="stylesheet" />
 </head>
 
